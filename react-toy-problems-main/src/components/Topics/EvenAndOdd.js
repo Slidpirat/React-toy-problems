bed600221dd36370c33eb06686f4ae7805f779dd
chrisdickerman93@gmail.com <you@example.com>
2022-02-24 22:23:36 -0700

    finished

---
+++ react-toy-problems-main/src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+export default class FilterObject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: "",
+    };
+  }
+
+  handleChange(change) {
+    this.setState({ userInput: change });
+    console.log(change);
+  }
+
+  handleClick(input) {
+    let array = input;
+    let even = [];
+    let odd = [];
+
+    for (let num = 0; num < array.length; num++) {
+      if (array[num] % 2 == 0) {
+        even.push(parseInt(array[num], 10));
+      } else {
+        odd.push(parseInt(array[num], 10));
+      }
+    }
+    this.setState({ evenArray: even, oddArray: odd });
+    console.log(even, odd);
+  }
+
+  render() {
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens and Odds</h4>
+        <input
+          className="inputLine"
+          onChange={(e) => {
+            this.handleChange(e.target.value);
+          }}
+        />
+        <button
+          className="confirmationButton"
+          onClick={() => {
+            this.handleClick(this.state.userInput);
+          }}
+        >
+          Submit here
+        </button>
+        <span className="resultsBox">evens: {this.state.evenArray}</span>
+        <span className="resultsBox">odds: {this.state.oddArray}</span>
+      </div>
+    );
+  }
+}
---
+++ react-toy-problems-main/src/components/Topics/Sum.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Sum extends Component {
+  render() {
+    return (
+      <div>
+        <p>Sum</p>
+      </div>
+    );
+  }
+}
